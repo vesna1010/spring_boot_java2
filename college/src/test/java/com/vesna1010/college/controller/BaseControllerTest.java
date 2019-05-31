147db1fe32cb6b1dc55183935a2f8b52cbb0d917
Vesna S <vesnasdoboj@gmail.com>
2019-05-31 04:35:53 +0200

    Update BaseControllerTest.java

--- college/src/test/java/com/vesna1010/college/controller/BaseControllerTest.java
+++ college/src/test/java/com/vesna1010/college/controller/BaseControllerTest.java
@@ -1,22 +1,14 @@
 package com.vesna1010.college.controller;
 
-import static org.springframework.security.test.web.servlet.setup.SecurityMockMvcConfigurers.springSecurity;
-import org.junit.Before;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.test.web.servlet.MockMvc;
-import org.springframework.test.web.servlet.setup.MockMvcBuilders;
-import org.springframework.web.context.WebApplicationContext;
 import com.vesna1010.college.CollegeApplicationTests;
 
+@AutoConfigureMockMvc
 public abstract class BaseControllerTest extends CollegeApplicationTests {
 
 	@Autowired
-	protected WebApplicationContext context;
 	protected MockMvc mockMvc;
-
-	@Before
-	public void setup() {
-		mockMvc = MockMvcBuilders.webAppContextSetup(context).apply(springSecurity()).build();
-	}
 
 }
