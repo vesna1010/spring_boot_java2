15fed0a0bf55710dd0ae305370b5e1ddbaeaa27b
Vesna <vesnasdoboj@gmail.com>
2019-08-11 08:45:26 +0200

    Update BaseServiceTest.java

--- college/src/test/java/com/vesna1010/college/service/BaseServiceTest.java
+++ college/src/test/java/com/vesna1010/college/service/BaseServiceTest.java
@@ -1,7 +1,12 @@
 package com.vesna1010.college.service;
 
-import com.vesna1010.college.CollegeApplicationTests;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+import com.vesna1010.college.BaseTest;
 
-public abstract class BaseServiceTest extends CollegeApplicationTests {
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public abstract class BaseServiceTest extends BaseTest {
 
 }
