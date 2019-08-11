13fa8376758ae62bbb65dd58e3190534d0017fec
Vesna <vesnasdoboj@gmail.com>
2019-08-11 08:44:55 +0200

    Update BaseRepositoryTest.java

--- college/src/test/java/com/vesna1010/college/repository/BaseRepositoryTest.java
+++ college/src/test/java/com/vesna1010/college/repository/BaseRepositoryTest.java
@@ -1,9 +1,14 @@
 package com.vesna1010.college.repository;
 
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.jdbc.Sql;
-import com.vesna1010.college.CollegeApplicationTests;
+import org.springframework.test.context.junit4.SpringRunner;
+import com.vesna1010.college.BaseTest;
 
+@RunWith(SpringRunner.class)
+@SpringBootTest
 @Sql(scripts = "classpath:init/MySQL.sql")
-public abstract class BaseRepositoryTest extends CollegeApplicationTests {
+public abstract class BaseRepositoryTest extends BaseTest {
 
 }
