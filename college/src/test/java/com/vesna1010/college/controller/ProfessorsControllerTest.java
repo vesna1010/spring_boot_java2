9f36121927e4f0d21010ed74d67aa7aee7940bd8
Vesna <vesnasdoboj@gmail.com>
2019-09-08 16:40:32 +0200

    Update ProfessorsControllerTest.java

--- college/src/test/java/com/vesna1010/college/controller/ProfessorsControllerTest.java
+++ college/src/test/java/com/vesna1010/college/controller/ProfessorsControllerTest.java
@@ -1,7 +1,6 @@
 package com.vesna1010.college.controller;
 
 import static org.hamcrest.CoreMatchers.is;
-import static org.hamcrest.CoreMatchers.nullValue;
 import static org.hamcrest.Matchers.hasProperty;
 import static org.hamcrest.collection.IsCollectionWithSize.hasSize;
 import static org.mockito.Mockito.doNothing;
@@ -19,6 +18,7 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 import java.time.LocalDate;
 import java.time.Month;
 import java.util.Arrays;
+import java.util.List;
 import org.junit.Test;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.data.domain.PageImpl;
@@ -40,14 +40,9 @@ public class ProfessorsControllerTest extends BaseControllerTest {
 	}
 
 	private void renderPageWithProfessorsNotLoggedIn() throws Exception {
-		when(service.findAllProfessors(PAGEABLE))
-				.thenReturn(new PageImpl<Professor>(Arrays.asList(professor1, professor2, professor3)));
-
 		mockMvc.perform(get("/professors"))
-                       .andExpect(status().is3xxRedirection())
-                       .andExpect(redirectedUrlPattern("**/login"));
-	
-		verify(service, times(0)).findAllProfessors(PAGEABLE);
+		       .andExpect(status().is3xxRedirection())
+		       .andExpect(redirectedUrlPattern("**/login"));
 	}
 
 	@Test
@@ -57,13 +52,8 @@ public class ProfessorsControllerTest extends BaseControllerTest {
 	}
 	
 	private void renderPageWithProfessorsNotAuthorized() throws Exception {
-		when(service.findAllProfessors(PAGEABLE))
-				.thenReturn(new PageImpl<Professor>(Arrays.asList(professor1, professor2, professor3)));
-
 		mockMvc.perform(get("/professors"))
-                       .andExpect(status().isForbidden());
-	
-		verify(service, times(0)).findAllProfessors(PAGEABLE);
+		       .andExpect(status().isForbidden());
 	}
 
 	@Test
@@ -79,14 +69,15 @@ public class ProfessorsControllerTest extends BaseControllerTest {
 	}
 	
 	private void renderPageWithProfessors() throws Exception {
-		when(service.findAllProfessors(PAGEABLE))
-				.thenReturn(new PageImpl<Professor>(Arrays.asList(professor1, professor2, professor3)));
+		List<Professor> professors = Arrays.asList(new Professor(1L, "Professor A"), new Professor(2L, "Professor B"));
+
+		when(service.findAllProfessors(PAGEABLE)).thenReturn(new PageImpl<Professor>(professors));
 
 		mockMvc.perform(get("/professors"))
-                       .andExpect(status().isOk())
-                       .andExpect(model().attribute("page", hasProperty("content", hasSize(3))))
-                       .andExpect(model().attribute("page", hasProperty("totalPages", is(1))))
-                       .andExpect(view().name("professors/page"));
+		       .andExpect(status().isOk())
+		       .andExpect(model().attribute("page", hasProperty("content", hasSize(2))))
+		       .andExpect(model().attribute("page", hasProperty("totalPages", is(1))))
+		       .andExpect(view().name("professors/page"));
 	
 		verify(service, times(1)).findAllProfessors(PAGEABLE);
 	}
@@ -98,17 +89,12 @@ public class ProfessorsControllerTest extends BaseControllerTest {
 	}
 
 	private void renderPageWithProfessorsByStudyProgrammeIdNotLoggedIn() throws Exception {
-		when(service.findAllProfessorsByStudyProgramId(1L, PAGEABLE))
-				.thenReturn(new PageImpl<Professor>(Arrays.asList(professor1, professor2)));
-
 		mockMvc.perform(
 				get("/professors")
 				.param("id", "1")
 				)
-                       .andExpect(status().is3xxRedirection())
-                       .andExpect(redirectedUrlPattern("**/login"));
-	
-		verify(service, times(0)).findAllProfessorsByStudyProgramId(1L, PAGEABLE);
+		       .andExpect(status().is3xxRedirection())
+		       .andExpect(redirectedUrlPattern("**/login"));
 	}
 	
 	@Test
@@ -118,16 +104,11 @@ public class ProfessorsControllerTest extends BaseControllerTest {
 	}
 	
 	private void renderPageWithProfessorsByStudyProgrammeIdNotAuthorized() throws Exception {
-		when(service.findAllProfessorsByStudyProgramId(1L, PAGEABLE))
-				.thenReturn(new PageImpl<Professor>(Arrays.asList(professor1, professor2)));
-
 		mockMvc.perform(
 				get("/professors")
 				.param("id", "1")
 				)
-                       .andExpect(status().isForbidden());
-	
-		verify(service, times(0)).findAllProfessorsByStudyProgramId(1L, PAGEABLE);
+		       .andExpect(status().isForbidden());
 	}
 
 	@Test
@@ -143,8 +124,9 @@ public class ProfessorsControllerTest extends BaseControllerTest {
 	}
 	
 	private void renderPageWithProfessorsByStudyProgrammeId() throws Exception {
-		when(service.findAllProfessorsByStudyProgramId(1L, PAGEABLE))
-				.thenReturn(new PageImpl<Professor>(Arrays.asList(professor1, professor2)));
+		List<Professor> professors = Arrays.asList(new Professor(1L, "Professor A"), new Professor(2L, "Professor B"));
+
+		when(service.findAllProfessorsByStudyProgramId(1L, PAGEABLE)).thenReturn(new PageImpl<Professor>(professors));
 
 		mockMvc.perform(
 				get("/professors")
@@ -152,8 +134,8 @@ public class ProfessorsControllerTest extends BaseControllerTest {
 				)
 		       .andExpect(status().isOk())
 		       .andExpect(model().attribute("page", hasProperty("content", hasSize(2))))
-                       .andExpect(model().attribute("page", hasProperty("totalPages", is(1))))
-                       .andExpect(view().name("professors/page"));
+		       .andExpect(model().attribute("page", hasProperty("totalPages", is(1))))
+		       .andExpect(view().name("professors/page"));
 	
 		verify(service, times(1)).findAllProfessorsByStudyProgramId(1L, PAGEABLE);
 	}
@@ -166,8 +148,8 @@ public class ProfessorsControllerTest extends BaseControllerTest {
 	
 	private void renderEmptyFormNotLoggedIn() throws Exception {
 		mockMvc.perform(get("/professors/form"))
-                       .andExpect(status().is3xxRedirection())
-                       .andExpect(redirectedUrlPattern("**/login"));
+		       .andExpect(status().is3xxRedirection())
+		       .andExpect(redirectedUrlPattern("**/login"));
 	}
 
 	@Test
@@ -178,7 +160,7 @@ public class ProfessorsControllerTest extends BaseControllerTest {
 	
 	private void renderEmptyFormNotAuthorized() throws Exception {
 		mockMvc.perform(get("/professors/form"))
-                       .andExpect(status().isForbidden());
+		       .andExpect(status().isForbidden());
 	}
 
 	@Test
@@ -195,9 +177,9 @@ public class ProfessorsControllerTest extends BaseControllerTest {
 
 	private void renderEmptyForm() throws Exception {
 		mockMvc.perform(get("/professors/form"))
-                       .andExpect(status().isOk())
-                       .andExpect(model().attribute("professor", is(new Professor())))
-                       .andExpect(view().name("professors/form"));
+		       .andExpect(status().isOk())
+		       .andExpect(model().attribute("professor", is(new Professor())))
+		       .andExpect(view().name("professors/form"));
 	}
 	
 	@Test
@@ -207,15 +189,9 @@ public class ProfessorsControllerTest extends BaseControllerTest {
 	}
 	
 	private void saveProfessorNotLoggedIn() throws Exception {
-		Professor professor = new Professor("Professor", "Parent", LocalDate.of(1985, Month.JANUARY, 6),
-				"profesor@gmail.com", "065 123 338", Gender.MALE, "Address", getPhoto(), "Title");
-
-		when(service.saveProfessor(professor))
-				.thenReturn(new Professor(4L, "Professor", "Parent", LocalDate.of(1995, Month.JANUARY, 6),
-						"profesor@gmail.com", "065 123 338", Gender.MALE, "Address", getPhoto(), "Title"));
-	
 		mockMvc.perform(
 				post("/professors/save")
+				.param("id", "1")
 				.param("name", "Professor")
 				.param("parent", "Parent")
 				.param("birthDate", "06.01.1985")
@@ -223,14 +199,12 @@ public class ProfessorsControllerTest extends BaseControllerTest {
 				.param("telephone", "065 123 338")
 				.param("gender", "MALE")
 				.param("address", "Address")
-				.param("photo", professor.getPhoto().toString())
+				.param("photo", getPhoto().toString())
 				.param("title", "Title")
 				.with(csrf())
 				)
 		       .andExpect(status().is3xxRedirection())
-                       .andExpect(redirectedUrlPattern("**/login"));
-		
-		verify(service, times(0)).saveProfessor(professor);
+		       .andExpect(redirectedUrlPattern("**/login"));
 	}
 	
 	@Test
@@ -240,15 +214,9 @@ public class ProfessorsControllerTest extends BaseControllerTest {
 	}
 	
 	private void saveProfessorNotAuthorized() throws Exception {
-		Professor professor = new Professor("Professor", "Parent", LocalDate.of(1985, Month.JANUARY, 6),
-				"profesor@gmail.com", "065 123 338", Gender.MALE, "Address", getPhoto(), "Title");
-
-		when(service.saveProfessor(professor))
-				.thenReturn(new Professor(4L, "Professor", "Parent", LocalDate.of(1995, Month.JANUARY, 6),
-						"profesor@gmail.com", "065 123 338", Gender.MALE, "Address", getPhoto(), "Title"));
-	
 		mockMvc.perform(
 				post("/professors/save")
+				.param("id", "1")
 				.param("name", "Professor")
 				.param("parent", "Parent")
 				.param("birthDate", "06.01.1985")
@@ -256,13 +224,11 @@ public class ProfessorsControllerTest extends BaseControllerTest {
 				.param("telephone", "065 123 338")
 				.param("gender", "MALE")
 				.param("address", "Address")
-				.param("photo", professor.getPhoto().toString())
+				.param("photo", getPhoto().toString())
 				.param("title", "Title")
 				.with(csrf())
 				)
 		       .andExpect(status().isForbidden());
-		
-		verify(service, times(0)).saveProfessor(professor);
 	}
 	
 	@Test
@@ -278,15 +244,14 @@ public class ProfessorsControllerTest extends BaseControllerTest {
 	}
 	
 	private void saveProfessor() throws Exception {
-		Professor professor = new Professor("Professor", "Parent", LocalDate.of(1985, Month.JANUARY, 6),
+		Professor professor = new Professor(1L, "Professor", "Parent", LocalDate.of(1985, Month.JANUARY, 6),
 				"profesor@gmail.com", "065 123 338", Gender.MALE, "Address", getPhoto(), "Title");
 
-		when(service.saveProfessor(professor))
-				.thenReturn(new Professor(4L, "Professor", "Parent", LocalDate.of(1995, Month.JANUARY, 6),
-						"profesor@gmail.com", "065 123 338", Gender.MALE, "Address", getPhoto(), "Title"));
-	
+		when(service.saveProfessor(professor)).thenReturn(professor);
+
 		mockMvc.perform(
 				post("/professors/save")
+				.param("id", "1")
 				.param("name", "Professor")
 				.param("parent", "Parent")
 				.param("birthDate", "06.01.1985")
@@ -294,13 +259,13 @@ public class ProfessorsControllerTest extends BaseControllerTest {
 				.param("telephone", "065 123 338")
 				.param("gender", "MALE")
 				.param("address", "Address")
-				.param("photo", professor.getPhoto().toString())
+				.param("photo", getPhoto().toString())
 				.param("title", "Title")
 				.with(csrf())
 				)
 		       .andExpect(model().hasNoErrors())
-                       .andExpect(status().is3xxRedirection())
-                       .andExpect(redirectedUrl("/professors/form"));
+		       .andExpect(status().is3xxRedirection())
+		       .andExpect(redirectedUrl("/professors/form"));
 		
 		verify(service, times(1)).saveProfessor(professor);
 	}
@@ -308,15 +273,14 @@ public class ProfessorsControllerTest extends BaseControllerTest {
 	@Test
 	@WithMockUser(authorities = "USER")
 	public void saveProfessorInvalidFormTest() throws Exception {
-		Professor professor = new Professor("Professor ??", "Parent", LocalDate.of(1985, Month.JANUARY, 6),
+		Professor professor = new Professor(1L, "Professor ??", "Parent", LocalDate.of(1985, Month.JANUARY, 6),
 				"profesor@gmail.com", "065 123 338", Gender.MALE, "Address", getPhoto(), "Title");
 
-		when(service.saveProfessor(professor))
-				.thenReturn(new Professor(4L, "Professor ??", "Parent", LocalDate.of(1995, Month.JANUARY, 6),
-						"profesor@gmail.com", "065 123 338", Gender.MALE, "Address", getPhoto(), "Title"));
-		
+		when(service.saveProfessor(professor)).thenReturn(professor);
+	
 		mockMvc.perform(
 				post("/professors/save")
+				.param("id", "1")
 				.param("name", "Professor ??")
 				.param("parent", "Parent")
 				.param("birthDate", "06.01.1985")
@@ -324,14 +288,14 @@ public class ProfessorsControllerTest extends BaseControllerTest {
 				.param("telephone", "065 123 338")
 				.param("gender", "MALE")
 				.param("address", "Address")
-				.param("photo", professor.getPhoto().toString())
+				.param("photo", getPhoto().toString())
 				.param("title", "Title")
 				.with(csrf())
 				)
 		       .andExpect(status().isOk())
-                       .andExpect(model().attributeHasFieldErrors("professor", "name"))
-                       .andExpect(model().attribute("professor", hasProperty("id", is(nullValue()))))
-                       .andExpect(view().name("professors/form"));
+		       .andExpect(model().attributeHasFieldErrors("professor", "name"))
+		       .andExpect(model().attribute("professor", is(professor)))
+		       .andExpect(view().name("professors/form"));
 		
 		verify(service, times(0)).saveProfessor(professor);
 	}
@@ -343,16 +307,12 @@ public class ProfessorsControllerTest extends BaseControllerTest {
 	}
 	
 	private void rederFormWithProfessorNotLoggedIn() throws Exception {
-		when(service.findProfessorById(1L)).thenReturn(professor1);
-		
 		mockMvc.perform(
 				get("/professors/edit")
 				.param("id","1")
 				)
-                       .andExpect(status().is3xxRedirection())
-                       .andExpect(redirectedUrlPattern("**/login"));
-		
-		verify(service, times(0)).findProfessorById(1L);
+		       .andExpect(status().is3xxRedirection())
+		       .andExpect(redirectedUrlPattern("**/login"));
 	}
 
 	@Test
@@ -361,16 +321,12 @@ public class ProfessorsControllerTest extends BaseControllerTest {
 		rederFormWithProfessorNotAuthorized();
 	}
 	
-	private void rederFormWithProfessorNotAuthorized() throws Exception {
-		when(service.findProfessorById(1L)).thenReturn(professor1);
-		
+	private void rederFormWithProfessorNotAuthorized() throws Exception {	
 		mockMvc.perform(
 				get("/professors/edit")
 				.param("id","1")
 				)
-                       .andExpect(status().isForbidden());
-		
-		verify(service, times(0)).findProfessorById(1L);
+		       .andExpect(status().isForbidden());
 	}
 
 	@Test
@@ -386,15 +342,15 @@ public class ProfessorsControllerTest extends BaseControllerTest {
 	}
 	
 	private void rederFormWithProfessor() throws Exception {
-		when(service.findProfessorById(1L)).thenReturn(professor1);
+		when(service.findProfessorById(1L)).thenReturn(new Professor(1L, "Professor"));
 		
 		mockMvc.perform(
 				get("/professors/edit")
 				.param("id","1")
 				)
 		       .andExpect(status().isOk())
-                       .andExpect(model().attribute("professor", hasProperty("name", is("Professor B"))))
-                       .andExpect(view().name("professors/form"));
+		       .andExpect(model().attribute("professor", hasProperty("name", is("Professor"))))
+		       .andExpect(view().name("professors/form"));
 		
 		verify(service, times(1)).findProfessorById(1L);
 	}
@@ -406,16 +362,12 @@ public class ProfessorsControllerTest extends BaseControllerTest {
 	}
 	
 	private void deleteProfessorByIdNotLoggedIn() throws Exception {
-		doNothing().when(service).deleteProfessorById(1L);
-		
 		mockMvc.perform(
 				get("/professors/delete")
 				.param("id", "1")
 				)
 		       .andExpect(status().is3xxRedirection())
-                       .andExpect(redirectedUrlPattern("**/login"));
-	
-		verify(service, times(0)).deleteProfessorById(1L);
+		       .andExpect(redirectedUrlPattern("**/login"));
 	}
 
 	@Test
@@ -425,15 +377,11 @@ public class ProfessorsControllerTest extends BaseControllerTest {
 	}
 	
 	private void deleteProfessorByIdNotAuthorized() throws Exception {
-		doNothing().when(service).deleteProfessorById(1L);
-		
 		mockMvc.perform(
 				get("/professors/delete")
 				.param("id", "1")
 				)
 		       .andExpect(status().isForbidden());
-	
-		verify(service, times(0)).deleteProfessorById(1L);
 	}
 
 	@Test
@@ -456,7 +404,7 @@ public class ProfessorsControllerTest extends BaseControllerTest {
 				.param("id", "1")
 				)
 		       .andExpect(status().is3xxRedirection())
-                       .andExpect(redirectedUrl("/professors"));
+		       .andExpect(redirectedUrl("/professors"));
 	
 		verify(service, times(1)).deleteProfessorById(1L);
 	}
