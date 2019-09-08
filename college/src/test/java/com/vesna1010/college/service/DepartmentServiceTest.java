cad7607a9ba3401327f88742ad97bde072963d78
Vesna <vesnasdoboj@gmail.com>
2019-09-08 15:51:07 +0200

    Update DepartmentServiceTest.java

--- college/src/test/java/com/vesna1010/college/service/DepartmentServiceTest.java
+++ college/src/test/java/com/vesna1010/college/service/DepartmentServiceTest.java
@@ -7,7 +7,6 @@ import static org.mockito.Mockito.doNothing;
 import static org.mockito.Mockito.times;
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
-import java.time.LocalDate;
 import java.util.Arrays;
 import java.util.List;
 import java.util.Optional;
@@ -29,7 +28,8 @@ public class DepartmentServiceTest extends BaseServiceTest {
 
 	@Test
 	public void findAllDepartmentsWithSortTest() {
-		when(repository.findAll(SORT)).thenReturn(Arrays.asList(department2, department1));
+		when(repository.findAll(SORT))
+				.thenReturn(Arrays.asList(new Department(1L, "Department A"), new Department(2L, "Department B")));
 
 		List<Department> departments = service.findAllDepartments(SORT);
 
@@ -41,45 +41,45 @@ public class DepartmentServiceTest extends BaseServiceTest {
 
 	@Test
 	public void findAllDepartmentsWithPageableTest() {
-		when(repository.findAll(PAGEABLE))
-				.thenReturn(new PageImpl<Department>(Arrays.asList(department1, department2)));
+		when(repository.findAll(PAGEABLE)).thenReturn(new PageImpl<Department>(
+				Arrays.asList(new Department(2L, "Department A"), new Department(2L, "Department B"))));
 
 		Page<Department> page = service.findAllDepartments(PAGEABLE);
 		List<Department> departments = page.getContent();
 
 		assertThat(page.getTotalPages(), is(1));
 		assertThat(departments, hasSize(2));
-		assertThat(departments.get(0).getName(), is("Department B"));
-		assertThat(departments.get(1).getName(), is("Department A"));
+		assertThat(departments.get(0).getName(), is("Department A"));
+		assertThat(departments.get(1).getName(), is("Department B"));
 		verify(repository, times(1)).findAll(PAGEABLE);
 	}
 
 	@Test
 	public void findDepartmentByIdTest() {
-		when(repository.findById(1L)).thenReturn(Optional.of(department1));
+		when(repository.findById(1L)).thenReturn(Optional.of(new Department(1L, "Department")));
 
 		Department department = service.findDepartmentById(1L);
 
-		assertThat(department.getName(), is("Department B"));
+		assertThat(department.getName(), is("Department"));
 		verify(repository, times(1)).findById(1L);
 	}
 
 	@Test(expected = RuntimeException.class)
 	public void findDepartmentByIdNotFoundTest() {
-		when(repository.findById(3L)).thenReturn(Optional.empty());
+		when(repository.findById(1L)).thenReturn(Optional.empty());
 
-		service.findDepartmentById(3L);
+		service.findDepartmentById(1L);
 	}
 
 	@Test
 	public void saveDepartmentTest() {
-		Department department = new Department("Department", LocalDate.now());
+		Department department = new Department(1L, "Department");
 
-		when(repository.save(department)).thenReturn(new Department(3L, "Department", LocalDate.now()));
+		when(repository.save(department)).thenReturn(department);
 
 		Department departmentSaved = service.saveDepartment(department);
 
-		assertThat(departmentSaved.getId(), is(3L));
+		assertThat(departmentSaved.getId(), is(1L));
 		verify(repository, times(1)).save(department);
 	}
 
