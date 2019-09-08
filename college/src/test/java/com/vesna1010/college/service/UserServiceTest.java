3ec37ed98f24a81f921c60db5cfaffc6eb0865b3
Vesna <vesnasdoboj@gmail.com>
2019-09-08 16:01:47 +0200

    Update UserServiceTest.java

--- college/src/test/java/com/vesna1010/college/service/UserServiceTest.java
+++ college/src/test/java/com/vesna1010/college/service/UserServiceTest.java
@@ -3,7 +3,6 @@ package com.vesna1010.college.service;
 import static org.hamcrest.CoreMatchers.is;
 import static org.hamcrest.collection.IsCollectionWithSize.hasSize;
 import static org.junit.Assert.assertThat;
-import static org.junit.Assert.assertTrue;
 import static org.mockito.Mockito.doNothing;
 import static org.mockito.Mockito.times;
 import static org.mockito.Mockito.verify;
@@ -16,117 +15,96 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageImpl;
-import org.springframework.security.crypto.password.PasswordEncoder;
-import com.vesna1010.college.enums.Authority;
-import com.vesna1010.college.models.User;
-import com.vesna1010.college.repositories.UserRepository;
-import com.vesna1010.college.services.UserService;
+import com.vesna1010.college.models.Subject;
+import com.vesna1010.college.repositories.SubjectRepository;
+import com.vesna1010.college.services.SubjectService;
 
-public class UserServiceTest extends BaseServiceTest {
+public class SubjectServiceTest extends BaseServiceTest {
 
 	@Autowired
-	private UserService service;
+	private SubjectService service;
 	@MockBean
-	private PasswordEncoder encoder;
-	@MockBean
-	private UserRepository repository;
+	private SubjectRepository repository;
 
 	@Test
-	public void findAllUsersTest() {
-		when(repository.findAll(PAGEABLE)).thenReturn(new PageImpl<User>(Arrays.asList(user1, user2, user3)));
+	public void findAllSubjectsByStudyProgramIdWithSortTest() {
+		when(repository.findAllByStudyProgramId(1L, SORT))
+				.thenReturn(Arrays.asList(new Subject(1L, "Subject A"), new Subject(2L, "Subject B")));
 
-		Page<User> page = service.findAllUsers(PAGEABLE);
-		List<User> users = page.getContent();
+		List<Subject> subjects = service.findAllSubjectsByStudyProgramId(1L, SORT);
+
+		assertThat(subjects, hasSize(2));
+		assertThat(subjects.get(0).getName(), is("Subject A"));
+		assertThat(subjects.get(1).getName(), is("Subject B"));
+		verify(repository, times(1)).findAllByStudyProgramId(1L, SORT);
+	}
+
+	@Test
+	public void findAllSubjectsWithPageableTest() {
+		when(repository.findAll(PAGEABLE)).thenReturn(
+				new PageImpl<Subject>(Arrays.asList(new Subject(1L, "Subject A"), new Subject(2L, "Subject B"))));
+
+		Page<Subject> page = service.findAllSubjects(PAGEABLE);
+		List<Subject> subjects = page.getContent();
 
 		assertThat(page.getTotalPages(), is(1));
-		assertThat(users, hasSize(3));
-		assertThat(users.get(0).getName(), is("User A"));
-		assertThat(users.get(1).getName(), is("User B"));
-		assertThat(users.get(2).getName(), is("User C"));
+		assertThat(subjects, hasSize(2));
+		assertThat(subjects.get(0).getName(), is("Subject A"));
+		assertThat(subjects.get(1).getName(), is("Subject B"));
 		verify(repository, times(1)).findAll(PAGEABLE);
 	}
 
 	@Test
-	public void findUserByIdTest() {
-		when(repository.findById(1L)).thenReturn(Optional.of(user1));
+	public void findAllSubjectsByStudyProgramIdWithPageableTest() {
+		when(repository.findAllByStudyProgramId(1L, PAGEABLE)).thenReturn(
+				new PageImpl<Subject>(Arrays.asList(new Subject(1L, "Subject A"), new Subject(2L, "Subject B"))));
 
-		User user = service.findUserById(1L);
+		Page<Subject> page = service.findAllSubjectsByStudyProgramId(1L, PAGEABLE);
+		List<Subject> subjects = page.getContent();
 
-		assertThat(user.getName(), is("User A"));
-		assertThat(user.getEmail(), is("userA@gmail.com"));
-		assertThat(user.getAuthority(), is(Authority.ADMIN));
-		assertTrue(user.isEnabled());
+		assertThat(page.getTotalPages(), is(1));
+		assertThat(subjects, hasSize(2));
+		assertThat(subjects.get(0).getName(), is("Subject A"));
+		assertThat(subjects.get(1).getName(), is("Subject B"));
+		verify(repository, times(1)).findAllByStudyProgramId(1L, PAGEABLE);
+	}
+
+	@Test
+	public void findSubjectByIdTest() {
+		when(repository.findById(1L)).thenReturn(Optional.of(new Subject(1L, "Subject")));
+
+		Subject subject = service.findSubjectById(1L);
+
+		assertThat(subject.getName(), is("Subject"));
 		verify(repository, times(1)).findById(1L);
 	}
 
 	@Test(expected = RuntimeException.class)
-	public void findUserByIdNotFoundTest() {
-		when(repository.findById(4L)).thenReturn(Optional.empty());
+	public void findSubjectByIdNotFoundTest() {
+		when(repository.findById(5L)).thenReturn(Optional.empty());
 
-		service.findUserById(4L);
+		service.findSubjectById(5L);
 	}
 
 	@Test
-	public void findUserByEmailTest() {
-		when(repository.findByEmail("userA@gmail.com")).thenReturn(Optional.of(user1));
+	public void saveSubjectTest() {
+		Subject subject = new Subject(1L, "Subject");
 
-		User user = service.findUserByEmail("userA@gmail.com");
+		when(repository.save(subject)).thenReturn(subject);
 
-		assertThat(user.getId(), is(1L));
-		assertThat(user.getName(), is("User A"));
-		assertThat(user.getAuthority(), is(Authority.ADMIN));
-		assertTrue(user.isEnabled());
-		verify(repository, times(1)).findByEmail("userA@gmail.com");
-	}
+		Subject subjectSaved = service.saveSubject(subject);
 
-	@Test(expected = RuntimeException.class)
-	public void findUserByEmailNotFoundTest() {
-		when(repository.findByEmail("userA@gmail.com")).thenReturn(Optional.empty());
-
-		service.findUserById(1L);
+		assertThat(subjectSaved.getId(), is(1L));
+		verify(repository, times(1)).save(subject);
 	}
 
 	@Test
-	public void saveUserTest() {
-		User user = new User("User", "user@gmail.com", "Password", Authority.USER);
-
-		when(encoder.encode("Password")).thenReturn("Encoded Password");
-		when(repository.save(user))
-				.thenReturn(new User(4L, "User", "user@gmail.com", "Encoded Password", Authority.USER));
-
-		User userSaved = service.saveUser(user);
-
-		assertThat(userSaved.getId(), is(4L));
-		verify(repository, times(1)).save(user);
-		verify(encoder, times(1)).encode("Password");
-	}
-
-	@Test
-	public void existsUserByEmailTest() {
-		when(repository.existsByEmail("userA@gmail.com")).thenReturn(true);
-
-		boolean isExistsUser = service.existsUserByEmail("userA@gmail.com");
-
-		assertTrue(isExistsUser);
-		verify(repository, times(1)).existsByEmail("userA@gmail.com");
-	}
-
-	@Test
-	public void deleteUserByIdTest() {
+	public void deleteSubjectByIdTest() {
 		doNothing().when(repository).deleteById(1L);
 
-		service.deleteUserById(1L);
+		service.deleteSubjectById(1L);
 
 		verify(repository, times(1)).deleteById(1L);
-	}
-
-	@Test
-	public void disableUserByIdTest() {
-		doNothing().when(repository).disableById(1L);
-
-		service.disableUserById(1L);
-
-		verify(repository, times(1)).disableById(1L);
 	}
 
 }
