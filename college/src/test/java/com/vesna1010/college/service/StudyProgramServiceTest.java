235d8c11e3e34d53a1bf46ec63eb173d92527285
Vesna <vesnasdoboj@gmail.com>
2019-09-08 16:00:43 +0200

    Update StudyProgramServiceTest.java

--- college/src/test/java/com/vesna1010/college/service/StudyProgramServiceTest.java
+++ college/src/test/java/com/vesna1010/college/service/StudyProgramServiceTest.java
@@ -7,7 +7,6 @@ import static org.mockito.Mockito.doNothing;
 import static org.mockito.Mockito.times;
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
-import java.time.LocalDate;
 import java.util.Arrays;
 import java.util.List;
 import java.util.Optional;
@@ -29,75 +28,73 @@ public class StudyProgramServiceTest extends BaseServiceTest {
 
 	@Test
 	public void findAllStudyProgramsWithSortTest() {
-		when(repository.findAll(SORT)).thenReturn(Arrays.asList(studyProgram3, studyProgram1, studyProgram2));
+		when(repository.findAll(SORT)).thenReturn(
+				Arrays.asList(new StudyProgram(1L, "Study Program A"), new StudyProgram(2L, "Study Program B")));
 
 		List<StudyProgram> studyPrograms = service.findAllStudyPrograms(SORT);
 
-		assertThat(studyPrograms, hasSize(3));
+		assertThat(studyPrograms, hasSize(2));
 		assertThat(studyPrograms.get(0).getName(), is("Study Program A"));
 		assertThat(studyPrograms.get(1).getName(), is("Study Program B"));
-		assertThat(studyPrograms.get(2).getName(), is("Study Program C"));
 		verify(repository, times(1)).findAll(SORT);
 	}
 
 	@Test
 	public void findAllStudyProgramsWithPageableTest() {
-		when(repository.findAll(PAGEABLE)).thenReturn(
-				new PageImpl<StudyProgram>(Arrays.asList(studyProgram1, studyProgram2, studyProgram3)));
+		when(repository.findAll(PAGEABLE)).thenReturn(new PageImpl<StudyProgram>(
+				Arrays.asList(new StudyProgram(1L, "Study Program A"), new StudyProgram(2L, "Study Program B"))));
 
 		Page<StudyProgram> page = service.findAllStudyPrograms(PAGEABLE);
 		List<StudyProgram> studyPrograms = page.getContent();
 
 		assertThat(page.getTotalPages(), is(1));
-		assertThat(studyPrograms, hasSize(3));
-		assertThat(studyPrograms.get(0).getName(), is("Study Program B"));
-		assertThat(studyPrograms.get(1).getName(), is("Study Program C"));
-		assertThat(studyPrograms.get(2).getName(), is("Study Program A"));
+		assertThat(studyPrograms, hasSize(2));
+		assertThat(studyPrograms.get(0).getName(), is("Study Program A"));
+		assertThat(studyPrograms.get(1).getName(), is("Study Program B"));
 		verify(repository, times(1)).findAll(PAGEABLE);
 	}
 
 	@Test
 	public void findAllStudyProgramsByDepartmentIdWithPageableTest() {
-		when(repository.findAllByDepartmentId(1L, PAGEABLE))
-				.thenReturn(new PageImpl<StudyProgram>(Arrays.asList(studyProgram1, studyProgram3)));
+		when(repository.findAllByDepartmentId(1L, PAGEABLE)).thenReturn(new PageImpl<StudyProgram>(
+				Arrays.asList(new StudyProgram(1L, "Study Program A"), new StudyProgram(2L, "Study Program B"))));
 
 		Page<StudyProgram> page = service.findAllStudyProgramsByDepartmentId(1L, PAGEABLE);
 		List<StudyProgram> studyPrograms = page.getContent();
 
 		assertThat(page.getTotalPages(), is(1));
 		assertThat(studyPrograms, hasSize(2));
-		assertThat(studyPrograms.get(0).getName(), is("Study Program B"));
-		assertThat(studyPrograms.get(1).getName(), is("Study Program A"));
+		assertThat(studyPrograms.get(0).getName(), is("Study Program A"));
+		assertThat(studyPrograms.get(1).getName(), is("Study Program B"));
 		verify(repository, times(1)).findAllByDepartmentId(1L, PAGEABLE);
 	}
 
 	@Test
 	public void findStudyProgramByIdTest() {
-		when(repository.findById(1L)).thenReturn(Optional.of(studyProgram1));
+		when(repository.findById(1L)).thenReturn(Optional.of(new StudyProgram(1L, "Study Program")));
 
 		StudyProgram studyProgram = service.findStudyProgramById(1L);
 
-		assertThat(studyProgram.getName(), is("Study Program B"));
+		assertThat(studyProgram.getName(), is("Study Program"));
 		verify(repository, times(1)).findById(1L);
 	}
 
 	@Test(expected = RuntimeException.class)
 	public void findStudyProgramByIdNotFoundTest() {
-		when(repository.findById(4L)).thenReturn(Optional.empty());
+		when(repository.findById(1L)).thenReturn(Optional.empty());
 
-		service.findStudyProgramById(4L);
+		service.findStudyProgramById(1L);
 	}
 
 	@Test
 	public void saveStudyProgramTest() {
-		StudyProgram studyProgram = new StudyProgram("StudyProgram", LocalDate.now(), 3, department1);
+		StudyProgram studyProgram = new StudyProgram(1L, "StudyProgram");
 
-		when(repository.save(studyProgram))
-				.thenReturn(new StudyProgram(4L, "StudyProgram", LocalDate.now(), 3, department1));
+		when(repository.save(studyProgram)).thenReturn(studyProgram);
 
 		StudyProgram studyProgramSaved = service.saveStudyProgram(studyProgram);
 
-		assertThat(studyProgramSaved.getId(), is(4L));
+		assertThat(studyProgramSaved.getId(), is(1L));
 		verify(repository, times(1)).save(studyProgram);
 	}
 
