cb5d4f361ff4f3d2c6ad37684f975b606aad02e3
Vesna <vesnasdoboj@gmail.com>
2019-09-08 15:51:55 +0200

    Update ProfessorServiceTest.java

--- college/src/test/java/com/vesna1010/college/service/ProfessorServiceTest.java
+++ college/src/test/java/com/vesna1010/college/service/ProfessorServiceTest.java
@@ -7,8 +7,6 @@ import static org.mockito.Mockito.doNothing;
 import static org.mockito.Mockito.times;
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
-import java.time.LocalDate;
-import java.time.Month;
 import java.util.Arrays;
 import java.util.List;
 import java.util.Optional;
@@ -17,7 +15,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageImpl;
-import com.vesna1010.college.enums.Gender;
 import com.vesna1010.college.models.Professor;
 import com.vesna1010.college.repositories.ProfessorRepository;
 import com.vesna1010.college.services.ProfessorService;
@@ -31,90 +28,86 @@ public class ProfessorServiceTest extends BaseServiceTest {
 
 	@Test
 	public void findAllProfessorsWithSortTest() {
-		when(repository.findAll(SORT)).thenReturn(Arrays.asList(professor3, professor1, professor2));
+		when(repository.findAll(SORT))
+				.thenReturn(Arrays.asList(new Professor(1L, "Professor A"), new Professor(2L, "Professor B")));
 
 		List<Professor> professors = service.findAllProfessors(SORT);
 
-		assertThat(professors, hasSize(3));
+		assertThat(professors, hasSize(2));
 		assertThat(professors.get(0).getName(), is("Professor A"));
 		assertThat(professors.get(1).getName(), is("Professor B"));
-		assertThat(professors.get(2).getName(), is("Professor C"));
 		verify(repository, times(1)).findAll(SORT);
 	}
 
 	@Test
 	public void findAllProfessorsByStudyProgramWithSortTest() {
 		when(repository.findAllDistinctBySubjectsStudyProgramId(1L, SORT))
-				.thenReturn(Arrays.asList(professor1, professor2));
+				.thenReturn(Arrays.asList(new Professor(1L, "Professor A"), new Professor(2L, "Professor B")));
 
 		List<Professor> professors = service.findAllProfessorsByStudyProgramId(1L, SORT);
 
 		assertThat(professors, hasSize(2));
-		assertThat(professors.get(0).getName(), is("Professor B"));
-		assertThat(professors.get(1).getName(), is("Professor C"));
+		assertThat(professors.get(0).getName(), is("Professor A"));
+		assertThat(professors.get(1).getName(), is("Professor B"));
 		verify(repository, times(1)).findAllDistinctBySubjectsStudyProgramId(1L, SORT);
 	}
 
 	@Test
 	public void findAllProfessorsWithPageableTest() {
-		when(repository.findAll(PAGEABLE))
-				.thenReturn(new PageImpl<Professor>(Arrays.asList(professor1, professor2, professor3)));
+		when(repository.findAll(PAGEABLE)).thenReturn(new PageImpl<Professor>(
+				Arrays.asList(new Professor(1L, "Professor A"), new Professor(2L, "Professor B"))));
 
 		Page<Professor> page = service.findAllProfessors(PAGEABLE);
 		List<Professor> professors = page.getContent();
 
 		assertThat(page.getTotalPages(), is(1));
-		assertThat(professors, hasSize(3));
-		assertThat(professors.get(0).getName(), is("Professor B"));
-		assertThat(professors.get(1).getName(), is("Professor C"));
-		assertThat(professors.get(2).getName(), is("Professor A"));
+		assertThat(professors, hasSize(2));
+		assertThat(professors.get(0).getName(), is("Professor A"));
+		assertThat(professors.get(1).getName(), is("Professor B"));
 		verify(repository, times(1)).findAll(PAGEABLE);
 	}
 
 	@Test
 	public void findAllProfessorsByStudyProgramWithPageableTest() {
-		when(repository.findAllDistinctBySubjectsStudyProgramId(1L, PAGEABLE))
-				.thenReturn(new PageImpl<Professor>(Arrays.asList(professor1, professor2)));
+		when(repository.findAllDistinctBySubjectsStudyProgramId(1L, PAGEABLE)).thenReturn(new PageImpl<Professor>(
+				Arrays.asList(new Professor(1L, "Professor A"), new Professor(2L, "Professor B"))));
 
 		Page<Professor> page = service.findAllProfessorsByStudyProgramId(1L, PAGEABLE);
 		List<Professor> professors = page.getContent();
 
 		assertThat(page.getTotalPages(), is(1));
 		assertThat(professors, hasSize(2));
-		assertThat(professors.get(0).getName(), is("Professor B"));
-		assertThat(professors.get(1).getName(), is("Professor C"));
+		assertThat(professors.get(0).getName(), is("Professor A"));
+		assertThat(professors.get(1).getName(), is("Professor B"));
 		verify(repository, times(1)).findAllDistinctBySubjectsStudyProgramId(1L, PAGEABLE);
 	}
 
 	@Test
 	public void findProfessorByIdTest() {
-		when(repository.findById(1L)).thenReturn(Optional.of(professor1));
+		when(repository.findById(1L)).thenReturn(Optional.of(new Professor(1L, "Professor")));
 
 		Professor professor = service.findProfessorById(1L);
 
-		assertThat(professor.getName(), is("Professor B"));
+		assertThat(professor.getName(), is("Professor"));
 		verify(repository, times(1)).findById(1L);
 	}
 
 	@Test(expected = RuntimeException.class)
 	public void findProfessorByIdNotFoundTest() {
-		when(repository.findById(4L)).thenReturn(Optional.empty());
+		when(repository.findById(1L)).thenReturn(Optional.empty());
 
-		service.findProfessorById(4L);
+		service.findProfessorById(1L);
 	}
 
 	@Test
 	public void saveStudentTest() {
-		Professor professor = new Professor("Professor", "Parent", LocalDate.of(1985, Month.JANUARY, 6),
-				"profesor@gmail.com", "065 123 338", Gender.MALE, "Address", getPhoto(), "Title");
+		Professor professor = new Professor(1L, "Professor");
 
-		when(repository.save(professor))
-				.thenReturn(new Professor(4L, "Professor", "Parent", LocalDate.of(1995, Month.JANUARY, 6),
-						"profesor@gmail.com", "065 123 338", Gender.MALE, "Address", getPhoto(), "Title"));
+		when(repository.save(professor)).thenReturn(professor);
 
 		Professor professorSaved = service.saveProfessor(professor);
 
-		assertThat(professorSaved.getId(), is(4L));
+		assertThat(professorSaved.getId(), is(1L));
 		verify(repository, times(1)).save(professor);
 	}
 
